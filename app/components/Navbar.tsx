08c2baa35557012647e1c29ee7aa0ca0ba8fbd04
Juarez Teixeira <x041699@gmail.com>
2026-01-04 18:13:59 -0400

    escrita logo1

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -103,7 +103,7 @@ export default function Navbar() {
             className="flex items-center gap-3"
           >
             <Image
-              src="/logo.png"
+              src="/logo1.png"
               alt="Solution Eletronic"
               width={56}
               height={56}
